45b5e28e3b7b6b5ca5146d3b984daa429890b7ee
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-12-16 14:55:22 +0530

    create sidebar

---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,9 @@
+function OverviewPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Overview</h1>
+    </div>
+  );
+}
+
+export default OverviewPage;
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,9 @@
+function ProductsPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Products</h1>
+    </div>
+  );
+}
+
+export default ProductsPage;
